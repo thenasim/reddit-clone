828987310c3c110ba76f9658729bf6898de1ddf3
Nasim Uddin <ficklenasim@outlook.com>
2021-01-06 19:43:31 +0600

    graphql server setup

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,29 @@
 require("dotenv").config();
 import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
-import { Post } from "./entities/post";
 import mikroConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up(); // run the migration
 
-  // const post = orm.em.create(Post, { title: "First post of reddit" });
-  // await orm.em.persistAndFlush(post);
+  const app = express();
 
-  // const posts = await orm.em.find(Post, {});
-  // console.table(posts);
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(process.env.PORT, () =>
+    console.log(`server started at http://localhost:${process.env.PORT}`)
+  );
 };
 
 main().catch(console.error);
